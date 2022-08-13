b4da1ed85eadca0231be5a8a3ea45e3805dfc17f
Aleksandr Bozhko <bozhko-aleksandr99@yandex.ru>
2022-08-13 17:20:18 +0300

    Разворот слов в строке, длина которых составляет 5 и более символов

---
+++ 4.py
@@ -0,0 +1,23 @@
+"""
+Write a function that takes in a string of one or more words, and returns the same string,
+    but with all five or more letter words reversed (Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+"""
+
+
+def spin_words(sentence):
+    lst = list(sentence.split())
+    rev = None
+    arr = []
+    for elem in range(len(lst)):
+        if len(lst[elem]) >= 5:
+            rev = lst[elem][::-1]
+            arr.append(rev)
+        else:
+            arr.append(lst[elem])
+    return " ".join(arr)
+
+
+string = 'Hi CodeWars'
+print(spin_words(string))
