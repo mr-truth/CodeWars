d57da2948ba1a08de5bf2abbc1774c0ce7f3740d
Aleksandr Bozhko <bozhko-aleksandr99@yandex.ru>
2022-08-15 00:28:56 +0300

    Нахождения суммы цифр числа с помощью рекурсии

---
+++ 8.py
@@ -0,0 +1,29 @@
+"""
+Digital root is the recursive sum of all the digits in a number.
+Given n, take the sum of the digits of n.
+If that value has more than one digit,
+continue reducing in this way until a single-digit number is produced.
+The input will be a non-negative integer.
+
+Examples:
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+"""
+
+
+def digital_root(n):
+    sum = 0
+    while n != 0:
+        digit = n % 10
+        sum += digit
+        n = n//10
+    if sum < 10:
+        return sum
+    return digital_root(sum)
+
+
+number = 678534
+print(digital_root(number))
+
